6998db80713494669fe7c7a16d47c96580040f5b
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-07-07 12:24:01 +0530

    Create Lecture19.cpp

---
+++ Lecture19.cpp
@@ -0,0 +1,9 @@
+//this lecture gives idea about memory allocation based on data types and binary representation
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	  long double b;
+	cout<<sizeof(a)<<endl;
+	cout<<sizeof(b)<<endl;
+}
