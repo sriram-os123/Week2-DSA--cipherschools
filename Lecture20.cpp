ebb6242178bf93c8b7825a25b8be30d9a99a81be
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-07-07 12:45:57 +0530

    Create Lecture20.cpp

---
+++ Lecture20.cpp
@@ -0,0 +1,10 @@
+//this lecture explains about memory representation in hexa decimal and gives idea how it works ,how it is useful.
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	  long double b;
+	cout<<sizeof(a)<<endl;
+	cout<<sizeof(b)<<endl;
+	cout<<&a<<" "<<&b<<endl;
+}
