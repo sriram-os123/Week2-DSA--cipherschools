1899fc7f8f1ca9d5233ca2a237a2ca2a955048a8
sriram os123 <118521565+sriram-os123@users.noreply.github.com>
2024-07-08 14:21:07 +0530

    Create Lecture21.cpp

---
+++ Lecture21.cpp
@@ -0,0 +1,9 @@
+//this lecture is about pointers basic explanation
+#include<iostream>
+using namespace std;
+int main(){
+	int a;
+	float b;
+	cout<<"the address of a"<<&a<<endl;
+	cout<<"the address of a"<<&b<<endl;
+}
